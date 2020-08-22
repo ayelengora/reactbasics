0adeecc7511f7cf5030f79fb7849b026a338a4e9
ayelen gora <ayegora@hotmail.com>
2020-08-22 16:44:50 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import PrimerComponente from './components/PrimerComponente';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Products from './components/Products';
+
+
 
 function App() {
-  const fecha = new Date().getFullYear()
+
   return (
-    <Fragment>
-      
-      <Header titulo="Tienda Virtual" />
-
-      <Products />
-
-      <Footer 
-          fecha={fecha}
-      />
-
-   </Fragment>
+    
   );
 }
 
--- src/components/PrimerComponente.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class PrimerComponente extends Component {
-  render() {
-    return (<h1> Soy PrimerComponente con class </h1>);
-  }
-}
-
-export default PrimerComponente;
